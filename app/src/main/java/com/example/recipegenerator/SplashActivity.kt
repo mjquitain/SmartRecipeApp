9e82266bfdbadaadedcff4a0a3102b2b643a60ea
Mykyla Jaimie Valenzuela <mjquitain5@gmail.com>
2026-01-30 03:32:38 +0800

    feat: Splash Screen and Sign In/Out UI

---
+++ app/src/main/java/com/example/recipegenerator/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.recipegenerator
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.recipegenerator.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
