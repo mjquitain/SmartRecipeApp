9d257a0aed9de1a201b60a6c41f7cf2b68aa5778
eth-cal <159922688+eth-cal@users.noreply.github.com>
2026-02-03 05:21:11 +0800

    [MIGRATION] Rebuilt older nav method using NavGraphs
    Also includes settings navigation.

---
+++ app/src/main/java/com/example/recipegenerator/navigation/NaviSetSettingsDestinations.kt
@@ -0,0 +1,28 @@
+package com.example.recipegenerator.navigation
+
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.example.recipegenerator.ui.screens.NotificationsScreen
+import com.example.recipegenerator.ui.screens.ProfileScreen
+
+
+fun NavGraphBuilder.naviSetSettingsDestinations(navigationNode : NavController) {
+    navigation(
+        startDestination = SettingsGraph.ProfileNode,
+        route = SettingsGraph::class
+    ) {
+        composable<SettingsGraph.ProfileNode> { ProfileScreen(
+            onBackClick = { navigationNode.popBackStack() },
+            navController = navigationNode
+        ) }
+
+        composable<SettingsGraph.NotificationsNode> { NotificationsScreen(
+            onBackClick = { navigationNode.popBackStack() }
+        ) }
+    }
+}
